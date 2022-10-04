8fa4844c710e16d73c25ee89d75d6c8646910897
Yuxuan <nine.slide@gmail.com>
2022-10-04 10:20:58 +0200

    render user list

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { useState } from 'react';
 import AddUser from './components/Users/AddUser';
-
+import UserList from './components/UserList';
 function App() {
-    let init = [{name:'Max', age:'(31 years old)'}];
+    let init = [{ name: 'Max', age: '(31 years old)' }];
     // const [users, setUsers] = useState(init)
     return (
         <div>
             <AddUser />
+            <UserList users={init} />
         </div>
     );
 }
--- src/components/UserList.js
+++ src/components/UserList.js
@@ -1,5 +1,17 @@
-import './UserList.scss'   
-
-function UserList() {};
+import classes from './UserList.module.css';
+import Card from './UI/Card';
+function UserList(props) {
+    return (
+        <Card className={classes.users}>
+            <ul>
+                {props.users.map((user) => (
+                    <li>
+                        {user.name} ({user.age} years old){' '}
+                    </li>
+                ))}
+            </ul>
+        </Card>
+    );
+}
 
 export default UserList;
