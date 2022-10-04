227b4279fb9c2e3b38ae3f3b41e1d1f2fde0c63a
Yuxuan <nine.slide@gmail.com>
2022-10-04 17:09:07 +0200

    use refs to replace state concepts of controlled and uncontrolled components

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,12 +1,17 @@
-import { useState } from 'react';
+import { useState,useRef } from 'react';
 import classes from './AddUser.module.css';
 import Card from '../UI/Card';
 import Button from '../UI/Button';
 import ErrorModal from '../UI/ErrorModal';
+//: refs is better when just reading a value while state is better for keylogger, it is called uncontrolled component if we use access values with ref 
 function AddNewUser(props) {
-    const [enteredUsername, setEnteredUsername] = useState('');
-    const [enteredAge, setEnteredAge] = useState('');
+    // const [enteredUsername, setEnteredUsername] = useState('');
+    // const [enteredAge, setEnteredAge] = useState('');
     const [error, setError] = useState()
+    const usernameRef = useRef()
+    const ageRef = useRef()
+    const enteredUsername = usernameRef.current.value
+    const enteredAge= ageRef.current.value
     function submitHandler(e) {
         e.preventDefault();
         if (
@@ -26,17 +31,19 @@ function AddNewUser(props) {
             return;
         }
         props.onAddUser(enteredUsername,enteredAge);
-        setEnteredUsername('');
-        setEnteredAge('');
+        // setEnteredUsername('');
+        // setEnteredAge('');
+        usernameRef.current.value=''
+        ageRef.current.value='' //.rarely use refs to manipulate the DOM
     }
 
-    const usernameChangeHandler = (e) => {
-        setEnteredUsername(e.target.value);
-    };
+    // const usernameChangeHandler = (e) => {
+    //     setEnteredUsername(e.target.value);
+    // };
 
-    const ageChangeHandler = (e) => {
-        setEnteredAge(e.target.value);
-    };
+    // const ageChangeHandler = (e) => {
+    //     setEnteredAge(e.target.value);
+    // };
 
     const errorHandler = ()=>{ //. to close modal window
         setError(null)
@@ -53,8 +60,9 @@ function AddNewUser(props) {
                     <input
                         id="username"
                         type="text"
-                        value={enteredUsername}
-                        onChange={usernameChangeHandler}
+                        // value={enteredUsername}
+                        // onChange={usernameChangeHandler}
+                        ref={usernameRef}
                     />
 
                     <label htmlFor="age">Age(Years)</label>
@@ -63,8 +71,9 @@ function AddNewUser(props) {
                         type="number"
                         min="0"
                         step="1"
-                        value={enteredAge}
-                        onChange={ageChangeHandler}
+                        // value={enteredAge}
+                        // onChange={ageChangeHandler}
+                        ref={ageRef} //.add ref property to access useRef
                     />
                     <Button type="submit"> Add User</Button>
                 </div>
