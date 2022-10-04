a8892ddbf022239011d9bcf3a224c2036adbd136
Yuxuan <nine.slide@gmail.com>
2022-10-04 15:53:39 +0200

    portal and fragment

--- src/components/UI/ErrorModal.js
+++ src/components/UI/ErrorModal.js
@@ -1,9 +1,17 @@
 import classes from './ErrorModal.module.css';
 import Card from './Card';
 import Button from './Button';
-function ErrorModal(props) {
+import ReactDOM from 'react-dom';
+const Backdrop = (props) => {
+    return <div
+            onClick={props.onClickDismiss}
+            className={classes.backdrop}
+        />
+};
+
+
+const ModalOverlay = (props) => {
     return (
-        <div onClick={props.onClickDismiss} className={classes.backdrop}>
         <Card className={classes.modal}>
             <header className={classes.header}>
                 <h2>{props.title}</h2>
@@ -12,12 +20,25 @@ function ErrorModal(props) {
                 <p>{props.msg}</p>
             </div>
             <footer className={classes.actions}>
-                <Button onClick={props.onClickDismiss}>oopsy</Button> 
+                <Button onClick={props.onClickDismiss}>oopsy</Button>
                 {/* //: the place where we want to click and close the modal */}
             </footer>
         </Card>
-        </div>
     );
+};
+function ErrorModal(props) {
+    return (
+    <> 
+    ReactDOM.createPortal(<Backdrop onClickDismiss={props.onClickDismiss} />,document.getElementById('backdrop-root'))
+    {ReactDOM.createPortal(
+        <ModalOverlay
+          title={props.title}
+          msg={props.msg} //! use the same variable name as the props.variable name
+          onClickDismiss={props.onClickDismiss} 
+        />,
+        document.getElementById('overlay-root')
+      )}
+    </>);
 }
 
 export default ErrorModal;
